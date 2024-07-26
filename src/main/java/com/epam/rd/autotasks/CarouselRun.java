fde45aafc049cada489e3dd328cf9a08d7db1b25
Rahul D <rahul_d@epam.com>
2024-07-26 17:28:08 +0530

    Made some improvements in terms of readability

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,31 +1,30 @@
 package com.epam.rd.autotasks;
 
-import java.lang.reflect.Array;
-import java.util.ArrayDeque;
-
+import static com.epam.rd.autotasks.DecrementingCarousel.carousalQue;
 import static com.epam.rd.autotasks.DecrementingCarousel.isRunning;
-import static com.epam.rd.autotasks.DecrementingCarousel.que;
 
 public class CarouselRun{
-
-    public CarouselRun() {
+    int capacity;
+    CarouselRun(int capacity)
+    {
+        this.capacity = capacity;
     }
 
     public int next() {
        int currentElement = -1 ;
-        if(que.isEmpty())
+        if(carousalQue.isEmpty())
         {
             return currentElement;
         }
        else {
-           currentElement = que.poll();
-           if(!(currentElement-1 <= 0)) que.offer(currentElement-1);
-           if(que.isEmpty()) isRunning = false;
+           currentElement = carousalQue.poll();
+           if(!(currentElement-1 <= 0)) carousalQue.offer(currentElement-1);
+           if(carousalQue.isEmpty()) isRunning = false;
        }
        return  currentElement;
     }
 
     public boolean isFinished() {
-        return que.isEmpty();
+        return carousalQue.isEmpty();
     }
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,79 +1,31 @@
 package com.epam.rd.autotasks;
-
 import java.util.ArrayDeque;
-import java.util.Scanner;
 
-public class DecrementingCarousel {
-    public static ArrayDeque<Integer> que;
-    public static boolean isRunning;
-    public static int capacity;
+
+public class DecrementingCarousel{
+
+    static boolean isRunning;
+    static ArrayDeque<Integer> carousalQue;
+    final int capacity;
 
     public DecrementingCarousel(int cap) {
-        que = new ArrayDeque<>();
-        capacity = cap;
         isRunning = false;
+        carousalQue = new ArrayDeque<>();
+        capacity = cap;
     }
 
     public boolean addElement(int element){
-        if(isRunning || que.size() == capacity || element <= 0) return false;
-        que.offer(element);
-        return true;
+        if(isRunning || carousalQue.size() == capacity || element <= 0) return false;
+        return carousalQue.offer(element);
     }
 
     public CarouselRun run() {
         if(!isRunning)
         {
             isRunning = true;
-            return new CarouselRun();
+            return new CarouselRun(capacity);
         }
         else return null;
     }
-
-//    public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//        DecrementingCarousel carousel = new DecrementingCarousel(3);
-//        CarouselRun carRun = null;
-//        int inp;
-//        while(true)
-//        {
-//            System.out.println("Press 1 to add Element");
-//            System.out.println("Press 2 to run carousal");
-//            System.out.println("Press 3 to do next");
-//            System.out.println("Press 4 to check Finished");
-//            inp = scan.nextInt();
-//            if(inp == 1)
-//            {
-//                int num = scan.nextInt();
-//                System.out.println(carousel.addElement(num));
-//            }
-//            else if(inp == 2)
-//            {
-//                if(carRun != null) System.out.println("Already Running");
-//                else
-//                {
-//                    carRun = carousel.run();
-//                    System.out.println("Run started");
-//                }
-//            }
-//            else if(inp == 3)
-//            {
-//                if(carRun != null)
-//                {
-//                    System.out.println(carRun.next());
-//                }
-//                else System.out.println("Carousal not running");
-//            }
-//            else if(inp == 4)
-//            {
-//                if(carRun != null)
-//                {
-//                    System.out.println(carRun.isFinished());
-//                }
-//                else{
-//                    System.out.println("Carousal not running");
-//                }
-//            }
-//        }
-//    }
 
 }
